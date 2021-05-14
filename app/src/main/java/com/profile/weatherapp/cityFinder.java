8da42bd30f22e7249055f01e9ba7f41b12404853
Ansh mishra <anshmishra9415@gmail.com>
2021-05-15 02:48:43 +0530

    weather app

---
+++ app/src/main/java/com/profile/weatherapp/cityFinder.java
@@ -0,0 +1,14 @@
+package com.profile.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cityFinder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_finder);
+    }
+}
